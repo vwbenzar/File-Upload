cdfe5cd81d6612f928ffc7e6b3fd2b7176654f30
Benzar <benzar@virtual-wonders.com>
2022-07-09 11:18:11 +0800

    fix pdf upload

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 			  
 		<form action="index.php" method="post" enctype="multipart/form-data">
 		  Select image to upload:
-		  <input type="file" name="fileToUpload" id="fileToUpload">
-		  <input type="submit" value="Upload Image" name="submit">
+		  <input type="file" name="fileToUpload" id="fileToUpload" >
+		  <input type="submit" value="Upload Image" name="submit" >
 		</form>
 	
 	</div>
@@ -27,15 +27,23 @@
 		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
 		  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-		  if($check !== false) {
-			echo "File is an image - " . $check["mime"] . ".";
+		  $check2 = $_FILES["fileToUpload"]["type"];
+		 
+		 if($check !== false) {
+			echo "File is an image - " . $check["mime"] . "."; 
 			$uploadOk = 1;
-		  } else {
+		  
+		  }else if($check2 == "application/pdf"){
+			echo "File is an pdf - " . $check2 . ".";
+		    $uploadOk = 1;
+		  
+		  }else {
 			echo "File is not an image.";
 			$uploadOk = 0;
-		  }
+		 
+		 }
 		}
-
+		
 		// Check if file already exists
 		if (file_exists($target_file)) {
 		  echo "Sorry, file already exists.";
@@ -43,15 +51,14 @@
 		}
 
 		// Check file size
-		if ($_FILES["fileToUpload"]["size"] > 500000) {
+		if ($_FILES["fileToUpload"]["size"] > 5000000) {
 		  echo "Sorry, your file is too large.";
 		  $uploadOk = 0;
 		}
 
 		// Allow certain file formats
-		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-		&& $imageFileType != "gif" ) {
-		  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		if($imageFileType != "jpg" && $imageFileType != "pdf" && $imageFileType != "jpeg" ) {
+		  echo "Sorry, only JPG, JPEG & PDF files are allowed.";
 		  $uploadOk = 0;
 		}
 
